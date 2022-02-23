b07e3d26032ec2cdea84e53b87a2606ecea710bc
Fernando Notebook <fcgraciano@gmail.com>
2022-02-23 09:53:33 -0300

    Add project files.

---
+++ Aula02_OrientacaoObjetos/Controllers/PessoaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aula02_OrientacaoObjetos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula02_OrientacaoObjetos.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public static List<Pessoa> lsPessoa = new List<Pessoa>(); 
+        public IActionResult Index()
+        {
+            return View(lsPessoa);
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult salvar(Pessoa item)
+        {
+            lsPessoa.Add(item);
+            return RedirectToAction("Index");
+        }
+    }
+}
